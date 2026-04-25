28295284ccc28ebb60b7992d953b124480043b05
gibsy35 <linkyourart@gmail.com>
2026-04-25 22:51:50 +0200

    Update main.tsx

--- FINAL/src/main.tsx
+++ FINAL/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CurrencyProvider } from "./context/CurrencyContext.tsx";
 import { LanguageProvider } from "./context/LanguageContext.tsx";
+import "./index.css";   // ← AJOUTE CETTE LIGNE ICI
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
