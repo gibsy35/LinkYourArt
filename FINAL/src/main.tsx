5a4373d1ffb49a06b48499876789e25efdb7ed74
gibsy35 <linkyourart@gmail.com>
2026-04-25 21:36:24 +0200

    Update main.tsx

--- FINAL/src/main.tsx
+++ FINAL/src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { LanguageProvider } from "./context/LanguageContext";
+import { CurrencyProvider } from "./context/CurrencyContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LanguageProvider>
-      <App />
+      <CurrencyProvider>
+        <App />
+      </CurrencyProvider>
     </LanguageProvider>
   </React.StrictMode>
 );
